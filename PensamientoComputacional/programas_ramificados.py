fb5f9f2e89a4055b5d42696c0a87bc58367ae244
Sebastian Quintero Arcila <squintero63@outlook.com>
2020-10-19 08:22:51 -0500

    Se agrega programas_ramificados.py

---
+++ PensamientoComputacional/programas_ramificados.py
@@ -0,0 +1,10 @@
+num_1 = int(input('Escoge un entero: '))
+num_2 = int(input('Escoge otro entero: '))
+
+if num_1 > num_2:
+    print('El primer número es MAYOR que el segundo!!')
+elif num_1 < num_2:
+    print('El primer número es MENOR que el segundo!!')
+else:
+    print('El primer número es IGUAL que el segundo!!')
+    
