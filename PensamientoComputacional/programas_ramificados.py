4d5b187deefd12c05f768f498bbe94baf37cd9ad
Sebastian Quintero Arcila <squintero63@outlook.com>
2020-10-19 08:43:03 -0500

    Ajuste en textos de impresión

--- PensamientoComputacional/programas_ramificados.py
+++ PensamientoComputacional/programas_ramificados.py
@@ -5,9 +5,9 @@ nombre_2 = input('Escribe el nombre número 2: ')
 edad_2 = int(input('Cuál es la edad del nombre número 2: '))
 
 if edad_1 > edad_2:
-    print(f'{nombre_1} " es mayor que " {nombre_2}')
+    print(f'{nombre_1} es mayor que {nombre_2}')
 elif edad_1 < edad_2:
-    print(f'{nombre_2} " es mayor que " {nombre_1}')
+    print(f'{nombre_2} es mayor que {nombre_1}')
 else:
-    print(f'{nombre_1} " y " {nombre_2} " tienen la misma edad"')
+    print(f'{nombre_1} y {nombre_2} tienen la misma edad')
     
