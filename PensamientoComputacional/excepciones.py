3b1111e22f33e27d5dfb584bff4027db13c9c26c
Sebastian Quintero Arcila <squintero63@outlook.com>
2020-11-13 14:53:22 -0500

    se agrega excepciones

---
+++ PensamientoComputacional/excepciones.py
@@ -0,0 +1,16 @@
+def run():
+    def divide_elementos_de_lista(lista, divisor):
+        try:
+            return [i / divisor for i in lista]
+        except ZeroDivisionError as e:
+            print(e)
+            return lista
+
+    lista = list(range(10))
+    divisor = 0
+
+    print(divide_elementos_de_lista(lista, divisor))
+
+
+if __name__ == '__main__':
+    run()
